19f3c8c0d00b13f0084d9fd2407a67d245ef0f5e
HBADE2002 <hrishikeshbade007@gmail.com>
2024-04-08 16:43:25 +0530

    new project using api requests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function getRandomQuote(quotes) { //function to get a random quote
+  return quotes[Math.floor(Math.random() * quotes.length)];//random quote from the array of quotes 
+} 
+
 
 function App() {
+  const [quotes, setQuotes] = useState([]); //array of quotes 
+  const [quote, setQuote] = useState(null); //quote to be displayed
+
+  function getRandomQuote(quotes) { //function to get a random quote
+    return quotes[Math.floor(Math.random() * quotes.length)];//random quote from the array of quotes 
+  }
+
+  useEffect(() => {
+    fetch('https://type.fit/api/quotes')
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Project 3: Quote Generator</h1>
+      <section>
+        <button onClick={getNewQuote}>New Quote</button>
+        <h3>
+          <span>"</span>
+          {quote?.text} 
+        </h3>
+        <i>- {quote?.author}</i>
+      </section>
+   </main>
   );
 }
 
